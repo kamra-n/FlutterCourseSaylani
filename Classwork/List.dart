1769d92ebf63232384b7033e854fa7e3856354aa
Kamran Ahmed Siddiqui <ikamihere@gmail.com>
2023-07-06 16:34:57 +0500

    assignment 03

---
+++ Classwork/List.dart
@@ -0,0 +1,16 @@
+void main() {
+  // updating List
+
+  // var myList = [0, 'one', 'two', 'three', 'Four', 'Five'];
+  // var checkList = myList.isNotEmpty;
+  // myList.replaceRange(1, 4, ['kamran', 'Ali']);
+  // print(myList);
+
+  // List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12];
+  // numbers.removeWhere((element) => element <= 10);
+  // print(numbers);
+
+  // Map<int, dynamic> fruits = {1: 'Apple', 2: 'Orange'};
+  // var name = fruits.putIfAbsent(2, () => 'Ali');
+  // print(name);
+}
